934899fe9115fd3badc686c80bf7d745538b171b
Stefano Ghirlanda <drghirlanda@gmail.com>
2014-12-25 00:52:21 -0500

    Renamed r to respond, added bind_model function, added some generalization matrices for rem and pearce 87/94, and some examples.

--- modsolve.py
+++ modsolve.py
@@ -1,7 +1,5 @@
 from sympy import *
 
-init_printing()
-
 ## define symbolic generalization matrix and associated symbols
 def define_g(i,j):
     g = globals()
@@ -39,12 +37,12 @@ def define_stimuli( S ):
     g[ 'R0' ] = Matrix( n, 1, n*[0] )
 
 ## define response function
-def r( Z, phase ):
+def respond( Z, phase ):
     g = globals()
     if phase == 0:
         return g[ 'R0' ][ Z ]
     myS = g[ 'S' + str(phase) ]
-    rZ = r( Z, phase - 1 );
+    rZ = respond( Z, phase - 1 );
     for i in myS:
         c = 'c' + str( g['S'][i] ) + str(phase)
         rZ += g[ c ] * G[i,Z]
@@ -59,7 +57,7 @@ def calculate_phase( phase, myS, myR ):
     R0 = g[ 'R' + str(phase-1) ]
     myR0 = Matrix( n, 1, n*[0] )
     for s in range(0, n):
-        myR0[ s ] = r( myS[s], phase-1 )
+        myR0[ s ] = respond( myS[s], phase-1 )
     myG = G[ myS, myS ]
     myC = []
     for s in myS:
@@ -74,16 +72,73 @@ def calculate_phase( phase, myS, myR ):
         pretty_print( sol[ c ] )
         print( "" )
 
-## define stimuli and associated objects
-define_stimuli( ['A', 'B', 'X', 'AB'] )
+## bind a result to a model 
+def G_rem():
+    g = globals()
+    r = symbols('r')
+    n = len( g['S'] )
+    G = Matrix( n, n, n**2 * [0] )
+    for i in range(0,n):
+        si = g['S'][ i ]
+        ni = len(si)
+        G[i,i] = ni
+        for j in range(i+1,n):
+            sj = g['S'][ j ]
+            nj = len(sj)
+            k = len( set(si) & set(sj) )
+            if k == 0:
+                gij= 0
+            else:
+                m = max( ni, nj ) - 1
+                gij = k * (1-r) ** m + (k-1)*r
+            G[i,j] = G[j,i] = gij
+    return G
 
-## phase 1: compound conditioning
-calculate_phase( 1, [AB, X], [1, 0] )
+## bind a result to a model 
+def bind_model( inexpr, G ):
+    g = globals()
+    n = len( g['S'] )
+    outexpr = inexpr
+    for i in range(0,n):
+        si = g['S'][ i ]
+        for j in range(i,n):
+            sj = g['S'][ j ]
+            outexpr = outexpr.subs( 'g'+si+'_'+sj, G[i,j] )
+    return outexpr.simplify()
 
-## phase 2: revaluation
-rA2 = symbols( 'rA2' )
-calculate_phase( 2, [A, X], [rA2, 0] )
+## model generalization factors
+rem = G_rem()
+c = symbols('c')
+pearce94 = Matrix( [ [1, c, 1/sqrt(2-c)],
+                  [c, 1, 1/sqrt(2-c)],
+                  [1/sqrt(2-c), 1/sqrt(2-c), 1] ] )
+pearce87 = Matrix( 3, 3, map( lambda x: x**2, pearce94 ) )
 
-rev = r( B, 2 ) - r( B, 1 )
-rev = rev.subs( [ (gB_X, gA_X), (gB_AB, gA_AB), (gX_X, 0) ] )
+## example: element reversal
+(rA1, rA2, rAB2) = symbols('rA1 rA2 rAB2')
+define_stimuli( ['A', 'B', 'AB'] )
+calculate_phase( 1, [A], [rA1] )
+calculate_phase( 2, [A, AB], [rA2, rAB2] )
+rB2 = respond( B, 2 )
+fr_rem = bind_model( rB2, rem )
+fr_p87 = bind_model( rB2, pearce87 )
+fr_p94 = bind_model( rB2, pearce94 )
+
+## example: external inhibition
+cleanup()
+define_stimuli( ['A', 'B', 'AB'] )
+calculate_phase( 1, [A], [1] )
+rB1 = respond( AB, 1 )
+ei_rem = bind_model( rB1, rem )
+ei_p87 = bind_model( rB1, pearce87 )
+ei_p94 = bind_model( rB1, pearce94 )
+
+## example: summation
+cleanup()
+define_stimuli( ['A', 'B', 'AB'] )
+calculate_phase( 1, [A, B], [1, 1] )
+rAB1 = respond( AB, 1 )
+sm_rem = bind_model( rAB1, rem )
+sm_p87 = bind_model( rAB1, pearce87 )
+sm_p94 = bind_model( rAB1, pearce94 )
 
