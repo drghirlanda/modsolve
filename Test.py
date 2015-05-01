b4b247a3dc536deec92de4b960bcb0014d6c285d
antonb03 <begeima@gmail.com>
2015-05-01 12:46:05 -0400

    just add tests to see if functions work

--- Test.py
+++ Test.py
@@ -2,11 +2,22 @@
 #no required in program
 import design
 import modsolve
+import rem
+import p87
+import p94
 d = design.design()
 d.append({'A':1, 'AB':0})
-
-print(d.printDesign())
 kernel = modsolve.kernelMachine()
-print d.allStimuli()
 kernel.train(d)
+rem = rem.rem(kernel)
+p87 = p87.p87(kernel)
+p94 = p94.p94(kernel)
+print
+
+p87.bind(kernel.symbols['wA1'])
+print
+rem.bind(kernel.symbols['wA1'])
+print
+p94.bind(kernel.symbols['wA1'])
+
 
