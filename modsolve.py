3f899d6b6267bdfc6cb0084c7ee2890e28d529b7
antonb03 <begeima@gmail.com>
2015-05-01 12:45:33 -0400

    functions for specific models were separated

--- modsolve.py
+++ modsolve.py
@@ -38,7 +38,7 @@ class kernelMachine(model):
                 if wXp not in s: 
                     s[ wXp ] = symbols( wXp ) # create symbol
                 ws.append( s[ wXp ] )         # append to weights list
-        print ws
+        #print ws
         ws = Matrix( ws )# turn list into vector
         iXs = []
         for i in design:
@@ -46,81 +46,13 @@ class kernelMachine(model):
                 iXs.append(self.stimuli.index(X)) # stimulus indices 
         G = self.G[ iXs, iXs ]            # generalization submatrix
         
+        
         Vs0 = Matrix([self.V(X, p-1) for i in design for X in i])    # initial Vs
-        Vs = Matrix( design.values() )                    # target Vs
-        solution = solve( Eq( Vs0 + G * ws, Vs ), ws ) # SOLUTION!
+        Vs = Matrix( design.values() )                   # target Vs
+        solution = solve( Eq( Vs0 + G * ws, Vs ), ws ) # SOLUTION
         for w in ws:
             self._info( str(w) + " = " + str( solution[w] ) )
             s[ str(w) ] = solution[ w ]   # add ws to symbols
-
-    def bind( self, inexpr, model ):
-        '''Compute the value of a generic expression for a particular
-        model.
-
-        '''
-        # the way we do this is to create a copy of the input
-        # expression, substitute all the gX_Y coefficients with the
-        # model values, and then simplify. the result is returned and
-        # NOT stored in the model's symbols dictionary.
-        s = self.symbols
-        self._make_G( model ) # create model's generalization matrix
-        G = s[ 'G' + model ]  # shortcut 
-        outexpr = inexpr      
-        n = len( self.stimuli )
-        for i in range(0, n):
-            si = self.stimuli[ i ]
-            for j in range(i, n):
-                sj = self.stimuli[ j ]
-                outexpr = outexpr.subs( 'g'+si+'_'+sj, G[i,j] )
-        outexpr = outexpr.simplify()
-        pretty_print( outexpr )
-        return outexpr
-
-    def _make_G( self, model ):
-        '''Create a generalization matrix for a given model.'''
-        s = self.symbols
-        if model == 'rem' and 'r' not in s:
-            s[ 'r' ] = symbols('r')
-        elif model in ['p87', 'p94'] and 'c' not in s:
-            s[ 'c' ] = symbols('c')
-        n = self.G.rows
-        G = s[ 'G'+model ] = Matrix( n, n, n**2 * [0] )
-        for i in range(0, G.rows):
-            s[ 'c' ] = symbols('c')
-            for j in range(i, G.rows ):
-                (si, sj) = str( self.G[i,j] )[ 1: ].split('_')
-                G[j,i] = G[i,j] = self._make_g( si, sj, model )
-
-    def _make_g( self, s1, s2, model ):
-        '''Create a single generalization factor for a given model.''' 
-        s = self.symbols
-        n1 = len( s1 )
-        n2 = len( s2 )
-        k = len( set(s1) & set(s2) )
-        if model == 'rem':
-            if k == 0:
-                return 0
-            elif s1 == s2:
-                return k
-            else:
-                m = max( n1, n2 ) - 1
-                g = k * ( 1 - s['r'] )**m + (k-1) * s['r']
-        elif model in ['p87', 'p94']:
-            if s1 == s2:
-                return 1
-            elif (n1==1 and n2==2 or n1==2 and n2==1) and k==1:
-                g = 1 / sqrt( 2 - s['c'] )
-            elif n1==1 and n2==1:
-                g = s['c']
-            else:
-                Exception( 'model "'+model+'" not implemented for '+
-                           str(s1)+' and '+str(s2) )
-            if model == 'p87':
-                g = g**2
-        else:
-            raise Exception('model "' + model + '" not known')
-        return g.simplify()
-
     def V( self, X, p ):
         '''Return the associative strength of X at the end of phase p.'''
         self._add_stimuli( [X] )
